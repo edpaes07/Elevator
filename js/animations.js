029f7e3ea893cd7733196dcc7d2b630e5ce31065
Daniel <danielrcavalcante@gmail.com>
2021-03-30 01:50:50 -0300

    elevador

--- js/animations.js
+++ js/animations.js
@@ -1,4 +1,5 @@
 const elevatorTime = 3000;
+var actualFloor = 0;
 
 let btns = document.querySelectorAll("#btn");
 
@@ -34,4 +35,49 @@ function openingDoors() {
     });
 }
 
-openingDoors();
+let avaliableImages = [
+    "https://media.discordapp.net/attachments/567559808085917705/826293432778489866/elevador1.jpg?width=601&height=450",
+    "https://media.discordapp.net/attachments/567559808085917705/826295479880777728/elevador2.jpg?width=592&height=450"
+]
+
+let floorVisor = document.querySelector("#visor-number");
+function goToFloor(floor){
+    
+    let avaliableFloors = [0,1,2,3];
+    let diference = floor - actualFloor;
+
+    let i = actualFloor;
+
+    function subir(){
+        setTimeout(function(){
+            if(i != floor){
+                i++;
+                actualFloor = i;
+                floorVisor.innerHTML = i;  
+                console.log(i);
+                subir();
+            }
+        }, 1000)
+    }
+
+    function descer(){
+        setTimeout(function(){
+            if(i != floor){
+                i--;
+                actualFloor = i;
+                floorVisor.innerHTML = i;  
+                if(i == 0) floorVisor.innerHTML = "T";
+                console.log(i);
+                descer();
+            }
+        }, 1000)
+    }
+
+    if(diference < 0){
+        descer();
+    }else{
+        subir();
+    } 
+
+    openingDoors();
+}
