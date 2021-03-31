3b9d4bd5a41e3cf50d306eae4b71d02e06b335d3
Daniel <danielrcavalcante@gmail.com>
2021-03-31 12:03:13 -0300

    leve refatoracao e portas abrindo na hora certa com a imagem certa

--- js/animations.js
+++ js/animations.js
@@ -4,35 +4,29 @@ var actualFloor = 0;
 let btns = document.querySelectorAll("#btn");
 
 function openingDoors() {
-
+    
     let leftDoor  = document.querySelector(".left-door");
     let rightDoor = document.querySelector(".right-door");
 
-    btns.forEach((button)=>{
-        button.addEventListener("click",(e)=>{
-            e.preventDefault();
+    leftDoor.classList.add('left-door-animation');
+    rightDoor.classList.add('right-door-animation');
 
-            leftDoor.classList.add('left-door-animation');
-            rightDoor.classList.add('right-door-animation');
-
+    setTimeout(() => {
+        leftDoor.classList.add('left-door');
+        rightDoor.classList.add('right-door');
+        leftDoor.classList.add('left-door-animation-opening');
+        rightDoor.classList.add('right-door-animation-opening');
+        setTimeout(() => {
+            leftDoor.classList.remove('left-door-animation');
+            rightDoor.classList.remove('right-door-animation');
             setTimeout(() => {
-                leftDoor.classList.add('left-door');
-                rightDoor.classList.add('right-door');
-                leftDoor.classList.add('left-door-animation-opening');
-                rightDoor.classList.add('right-door-animation-opening');
-                setTimeout(() => {
-                    leftDoor.classList.remove('left-door-animation');
-                    rightDoor.classList.remove('right-door-animation');
-                    setTimeout(() => {
-                        leftDoor.classList.remove('left-door-animation-opening');
-                        rightDoor.classList.remove('right-door-animation-opening');
-                    }, 1000);
-                }, 10);
-            }, elevatorTime);
+                leftDoor.classList.remove('left-door-animation-opening');
+                rightDoor.classList.remove('right-door-animation-opening');
+            }, 1000);
+        }, 10);
+    }, elevatorTime);
 
             
-        });     
-    });
 }
 
 let avaliableImages = [
@@ -40,44 +34,63 @@ let avaliableImages = [
     "https://media.discordapp.net/attachments/567559808085917705/826295479880777728/elevador2.jpg?width=592&height=450"
 ]
 
-let floorVisor = document.querySelector("#visor-number");
+function setImageByFloor(){
+
+    background = document.querySelector(".black-box");
+
+    switch (actualFloor) {
+        case 3:
+            background.style.backgroundImage = `url(${avaliableImages[0]})`;
+            break;
+        case 2:
+            background.style.backgroundImage = `url(${avaliableImages[1]})`;
+            break;
+        case 1:
+            background.style.backgroundImage = `url(${avaliableImages[0]})`;
+            break;
+        case 0:
+            background.style.backgroundImage = `url(${avaliableImages[1]})`;
+            break;
+    }
+
+} 
+
 function goToFloor(floor){
     
-    let avaliableFloors = [0,1,2,3];
     let diference = floor - actualFloor;
-
+    let floorVisor = document.querySelector("#visor-number");
     let i = actualFloor;
 
-    function subir(){
-        setTimeout(function(){
+    function upELevator(){
+        setTimeout(() => {
             if(i != floor){
                 i++;
                 actualFloor = i;
                 floorVisor.innerHTML = i;  
-                console.log(i);
-                subir();
-            }
+                if(actualFloor == floor) openingDoors();
+                setImageByFloor();
+                upELevator();
+            } 
         }, 1000)
     }
 
-    function descer(){
-        setTimeout(function(){
+    function downElevator(){
+        setTimeout(() => {
             if(i != floor){
                 i--;
                 actualFloor = i;
                 floorVisor.innerHTML = i;  
                 if(i == 0) floorVisor.innerHTML = "T";
-                console.log(i);
-                descer();
+                if(actualFloor == floor) openingDoors();
+                setImageByFloor();
+                downElevator();
             }
         }, 1000)
     }
 
     if(diference < 0){
-        descer();
+        downElevator();
     }else{
-        subir();
+        upELevator();
     } 
-
-    openingDoors();
 }
